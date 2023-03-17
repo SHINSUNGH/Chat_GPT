db199e46195d637360b77138004c991eee88e3c0
SHINSUNGH <shin1235631@gmail.com>
2023-03-17 15:31:01 +0900

    asd

---
+++ ssh.py
@@ -0,0 +1,24 @@
+# 리스트 생성
+numbers = [1, 2, 3, 4, 5]
+
+# 반복문을 이용하여 리스트의 요소 출력
+for num in numbers:
+    print(num)
+
+# 딕셔너리 생성
+person = {'name': 'John', 'age': 30, 'city': 'New York'}
+
+# 딕셔너리의 값 출력
+print(person['name'])
+print(person['age'])
+print(person['city'])
+
+# 함수 정의
+def add_numbers(x, y):
+    return x + y
+
+# 함수 호출
+result = add_numbers(5, 10)
+
+# 결과 출력
+print(result)
